31468db601a832ea6bf383815fc91d9f07bc1d57
Vladg-Note <gricenkovlad007@gmail.com>
2025-09-30 15:46:54 +0300

    Add to_upper func

---
+++ src/to_upper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void *to_upper(const char *str) {
+    if (str == NULL) {
+        return NULL;
+    }
+
+    // !!!ЗАМЕНИТЬ НА strlen!!!
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    char *result = malloc(len + 1);
+    if (result == NULL) {
+        return NULL; 
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        result[i] = (char)toupper((unsigned char)str[i]);
+    }
+    result[len] = '\0'; 
+
+    return result;
+}
---
+++ src/to_upper.h
@@ -0,0 +1,11 @@
+#ifndef TO_UPPER_H
+#define TO_UPPER_H
+
+#include <stddef.h> // !Заменить на наш NULL и size_t
+
+/*
+* Возвращает копию строки (str), преобразованной в верхний регистр.
+*/
+void *to_upper(const char *str);
+
+#endif /* TO_LOWER */
