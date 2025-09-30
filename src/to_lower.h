c1891b567b792fd8634747b500414c7800af6e1f
Vladg-Note <gricenkovlad007@gmail.com>
2025-09-30 15:46:13 +0300

    Add to_lower func

---
+++ src/to_lower.h
@@ -0,0 +1,11 @@
+#ifndef TO_LOWER_H
+#define TO_LOWER_H
+
+#include <stddef.h> // !Заменить на наш NULL и size_t
+
+/*
+* Возвращает копию строки (str), преобразованной в верхний регистр.
+*/
+void *to_lower(const char *str);
+
+#endif /* TO_LOWER */
